6fed67fb9136b1b2d8a7c96ef2c79a64d3b25f73
Christian Q. Cleofas <cleofastiano27@gmail.com>
2024-03-10 08:03:43 +0800

    Add project files.

---
+++ Consolidation-System/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePicker1.ValueChanged
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DateTimePicker1.CustomFormat = "MM/dd/yyyy"
+        DateTimePicker1.Format = DateTimePickerFormat.Custom
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub txtTotalOrders_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtTotalOrders.KeyPress
+        If e.KeyChar = ChrW(Keys.Enter) Then
+            lblPercent.Text = Val(txtTotalOrders.Text) / 100
+            lblConviFee.Text = Val(lblPercent.Text) * 5
+            lblTotalSales.Text = Val(txtTotalOrders.Text) - Val(lblConviFee.Text)
+        End If
+    End Sub
+End Class
---
+++ Consolidation-System/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Consolidation-System")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("HP")>
+<Assembly: AssemblyProduct("Consolidation-System")>
+<Assembly: AssemblyCopyright("Copyright © HP 2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("050adff0-740f-4b37-9290-c6b925ee8f1c")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
